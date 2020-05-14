3f890cbcd383cc542b2a8fbf0bfbd9ea16e9846f
pawelpustula <pawelpustula98@gmail.com>
2020-05-14 19:58:59 +0200

    Piłkarze_MVVM

---
+++ Piłkarze_MVVM/Piłkarze_MVVM/Model/Team.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Piłkarze_MVVM.Model
+{
+    class Team
+    {
+        public List<Player> GetPlayers { get; } = ReadPlayersFromFile(@"Players.json");
+
+        public void AddPlayer(Player player)
+        {
+            GetPlayers.Add(player);
+        }
+
+        public void EditPlayer(Player player, int indexOfPlayer)
+        {
+            GetPlayers[indexOfPlayer] = player;
+        }
+
+        public void DeletePlayer(int indexOfPlayer)
+        {
+            GetPlayers.RemoveAt(indexOfPlayer);
+        }
+
+        public bool CheckIfPlayerIsOnList(Player player)
+        {
+            int index = 0;
+            bool playerIsOnList = false;
+
+            while(index < GetPlayers.Count && !playerIsOnList)
+            {
+                if (GetPlayers[index].isTheSameAs(player))
+                {
+                    playerIsOnList = true;
+                }
+                index++;
+            }
+
+            return playerIsOnList;
+
+            //foreach (Player p in GetPlayers)
+            //{
+            //    if (p.isTheSameAs(player))
+            //    {
+            //        playerIsOnList = true;
+            //        break;
+            //    }
+            //}
+            //return playerIsOnList;
+        }
+
+        public void SavePlayersToFile(string file)
+        {
+            string text = JsonConvert.SerializeObject(GetPlayers);
+            File.WriteAllText(file, text);
+        }
+
+        public static List<Player> ReadPlayersFromFile(string file)
+        {
+            List<Player> players = new List<Player>();
+            if (!File.Exists(file))
+            {
+                throw new Exception("File does not exist.");
+            }
+            else
+            {
+                players = JsonConvert.DeserializeObject<List<Player>>(File.ReadAllText(file));
+                return players;
+            }
+        }
+    }
+}
---
+++ Piłkarze_MVVM/Piłkarze_MVVM/VievModel/BaseClass/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+
+namespace Piłkarze_MVVM.VievModel.BaseClass
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void onPropertyChanged(params string[] namesOfProperties)
+        {
+            if (PropertyChanged != null)
+            {
+                foreach (var prop in namesOfProperties)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(prop));
+                }
+            }
+        }
+    }
+}
